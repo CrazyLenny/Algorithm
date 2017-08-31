79d1b35c9d6d38ecfdc2d8a1d87de3dfecea6ef7
CrazyLenny <ykxlenny@gmail.com>
2017-08-31 09:50:16 -0400

    Add print command.

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 31, 2017
+ *      Author: Kexin
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello Algorithm!" << endl;
+
+}
